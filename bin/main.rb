d42f61344526f8dd577f5fe778b24e68234a7cfb
Oscar Mier <voscarmv@gmail.com>
2020-03-04 16:07:23 -0600

    Corrected linter errors.

--- bin/main.rb
+++ bin/main.rb
@@ -1,67 +1,59 @@
 #!/usr/bin/env ruby is the top line
+# rubocop : disable Metrics/BlockLength
 exit = false
 
-until exit do
+until exit
   puts "Welcome to Luis and Oscar's Tic Tac Toe (All rights reserved 2020)"
-  while true do
-    puts "Player 1 (X) name:"
+  loop do
+    puts 'Player 1 (X) name:'
     p1name = gets.chomp
-    if p1name == '' then
-      puts "Enter a valid name."
-      next
-    else
-      break
-    end
-    # player_1 = player.new(p1name)
+    break unless p1name == ''
+
+    puts 'Enter a valid name.'
   end
 
-  while true do
-    puts "Player 2 (O) name:"
+  loop do
+    puts 'Player 2 (O) name:'
     p2name = gets.chomp
-    if p2name == '' then
-      puts "Enter a valid name."
-      next
-    else
-      break
-    end
-    # player_2 = player.new(p2name)
+    break unless p2name == ''
+
+    puts 'Enter a valid name.'
   end
 
   full_board = false
   player_1_wins = false
   player_2_wins = false
-  while true do
+  valid_coords = %w[1 2 3]
+  loop do
     #
     # PLAYER 1 TURN
     #
 
     space_occupied = true
-    while space_occupied do
-      while true do
-        puts "Player 1: Select a row position between 1 and 3"
-        play_row_1 = gets.chomp
-        unless play_row_1 == "1" || play_row_1 == "2" || play_row_1 == "3"
-          puts "Enter a valid number."
+    while space_occupied
+      loop do
+        puts 'Player 1: Select a row position between 1 and 3'
+        player_1_row = gets.chomp
+        unless valid_coords.include?(player_1_row)
+          puts 'Enter a valid number.'
           next
         end
         break
       end
 
-      while true do
-        puts "Player 1: Select a column position between 1 and 3"
-        play_col_1 = gets.chomp
-        unless play_col_1 == "1" || play_col_1 == "2" || play_col_1 == "3"
-          puts "Enter a valid number."
+      loop do
+        puts 'Player 1: Select a column position between 1 and 3'
+        player_1_col = gets.chomp
+        unless valid_coords.include?(player_1_col)
+          puts 'Enter a valid number.'
           next
         end
         break
       end
 
-      # space_occupied = board.occupied?(play_col_1, play_row_1)
+      # space_occupied = board.occupied?(player_1_col, player_1_row)
       space_occupied = false
-      if space_occupied 
-        puts "Square #{play_col_1}, #{play_row_1} is occupied. Choose an empty square."
-      end
+      puts "Square #{player_1_col}, #{player_1_row} is occupied. Choose an empty square." if space_occupied
 
     end
 
@@ -69,7 +61,7 @@ until exit do
 
     # player_1_wins = player_1.victory
     if player_1_wins == true
-      puts "Player 1 wins the match!"
+      puts 'Player 1 wins the match!'
       break
     end
     # full_board = board.isfull()
@@ -83,32 +75,30 @@ until exit do
     #
 
     space_occupied = true
-    while space_occupied do
-      while true do
-        puts "Player 2: Select a row position between 1 and 3"
-        play_row_2 = gets.chomp
-        unless play_row_2 == "1" || play_row_2 == "2" || play_row_2 == "3"
-          puts "Enter a valid number."
+    while space_occupied
+      loop do
+        puts 'Player 2: Select a row position between 1 and 3'
+        player_2_row = gets.chomp
+        unless valid_coords.include?(player_2_row)
+          puts 'Enter a valid number.'
           next
         end
         break
       end
 
-      while true do
-        puts "Player 2: Select a column position between 1 and 3"
-        play_col_2 = gets.chomp
-        unless play_col_2 == "1" || play_col_2 == "2" || play_col_2 == "3"
-          puts "Enter a valid number."
+      loop do
+        puts 'Player 2: Select a column position between 1 and 3'
+        player_2_col = gets.chomp
+        unless valid_coords.include?(player_2_col)
+          puts 'Enter a valid number.'
           next
         end
         break
       end
 
-      # space_occupied = board.occupied?(play_col_2, play_row_2)
+      # space_occupied = board.occupied?(player_2_col, player_2_row)
       space_occupied = false
-      if space_occupied 
-        puts "Square #{play_col_2}, #{play_row_2} is occupied. Choose an empty square."
-      end
+      puts "Square #{player_2_col}, #{player_2_row} is occupied. Choose an empty square." if space_occupied
 
     end
 
@@ -117,7 +107,7 @@ until exit do
     # player_1_wins = player_1.victory
     player_2_wins = true
     if player_2_wins == true
-      puts "Player 2 wins the match!"
+      puts 'Player 2 wins the match!'
       break
     end
     # full_board = board.isfull()
@@ -125,20 +115,20 @@ until exit do
       puts "It's a draw..."
       break
     end
-
   end
 
-  puts "Play again? (Y/N)"
+  puts 'Play again? (Y/N)'
   exit = gets.chomp
   case exit.upcase
-  when "N"
+  when 'N'
     exit = true
-  when "Y"
-    puts "Bye!"
+  when 'Y'
+    puts 'Bye!'
     exit = false
-  else 
-    puts "Wrong option."
-    puts "Exit game? (Y/N)"
-    exit = gets.chomp  
+  else
+    puts 'Wrong option.'
+    puts 'Exit game? (Y/N)'
+    exit = gets.chomp
   end
 end
+# rubocop : enable Metrics/BlockLength
